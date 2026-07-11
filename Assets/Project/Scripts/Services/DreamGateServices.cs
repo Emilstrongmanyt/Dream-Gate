cc62442bbb2072248d62129aaae641d9d41bbdc0
Emil Jensen <emilstrongmanyt@gmail.com>
2026-07-11 05:37:02 +0200

    Fix CI compile: valid SupabaseAuthNative meta GUID, colocate PhoneAuthService

--- Assets/Project/Scripts/Services/DreamGateServices.cs
+++ Assets/Project/Scripts/Services/DreamGateServices.cs
@@ -28,7 +28,9 @@ namespace DreamGate.Battlegrounds.Services
                 _ = CloudCoroutineHost.Instance;
                 AppleSignInNative.Warmup();
                 GoogleSignInNative.Warmup();
+#if UNITY_IOS && !UNITY_EDITOR
                 SupabaseAuthNative.Warmup();
+#endif
                 CloudClient ??= new SupabaseClient(settings, CloudCoroutineHost.Instance);
                 if (CloudClient.IsAuthenticated)
                 {
